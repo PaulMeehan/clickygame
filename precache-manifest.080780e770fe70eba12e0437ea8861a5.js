afc4e07fe5d5581124818c1693a305d252eddc55
PaulM <pmeehan456@gmail.com>
2019-04-28 17:16:16 -0400

    Updates

---
+++ precache-manifest.080780e770fe70eba12e0437ea8861a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836f2cb0ef1ab97109008bd0c2736d0e",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "e161ea0fb17287c6c1af",
+    "url": "/clickygame/static/css/main.78fe19b4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickygame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e161ea0fb17287c6c1af",
+    "url": "/clickygame/static/js/main.bf2f5b55.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
